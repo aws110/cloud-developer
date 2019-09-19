3359fb07ed63c2c1593635071720bde2e2d45700
Adam Simonar <adamsimonar@adams-mbp.gateway.sasktel.net>
2019-09-19 15:59:56 -0600

    Adding proper files for exercise 2 in course 3

--- course-03/exercises/udacity-c3-restapi-user/src/config/config.ts
+++ course-03/exercises/udacity-c3-restapi-user/src/config/config.ts
@@ -7,8 +7,7 @@ export const config = {
     "dialect": "postgres",
     "aws_reigion": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET,
-    "url": process.env.URL
+    "aws_media_bucket": process.env.AWS_BUCKET
   },
   "prod": {
     "username": "",
